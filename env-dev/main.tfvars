1565c3fe86c98519102d9b4d90f009720c3f8e63
sunil <sunilkumar.ipm@gmail.com>
2023-05-24 21:35:38 +0530

    learn-Ansible parameter

---
+++ env-dev/main.tfvars
@@ -0,0 +1,6 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.devops2023sk.online:8080/" },
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.devops2023sk.online:8080/" }
+
+
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test2"
-  value = "Hello world"
+resource "aws_ssm_parameter" "parameters" {
+  count =  length(var.parameters)
+  name  =  var.parameters[count.index].name
+  value =  var.parameters[count.index].value
   type  = "SecureString"
   key_id = "d8114269-4589-4ff0-b070-3df5d1a3c910"
 }
