f292b77107099c7876153c6970e255c0a0e82266
sunil <sunilkumar.ipm@gmail.com>
2023-05-24 21:16:23 +0530

    learn-Ansible parameter

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test2"
-  type  = "String"
   value = "Hello world"
+  type  = "SecureString"
+  key_id = "d8114269-4589-4ff0-b070-3df5d1a3c910"
 }
